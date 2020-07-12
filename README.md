35441b63da0908d6a27189ec5c7b01830a3f1c57
swon3210 <swon3210@gmail.com>
2020-07-12 18:16:09 +0900

    관리 뷰와 기능 추가됨

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 소개
 
-Bootstrap + Node.js + MySQL 으로 제작되었습니다.
+Bootstrap + Node.js + MySQL 으로 제작되었습니다. 반드시 bookdatabase 라는 이름의 mysql 데이터베이스가 생성되어 있는 상태여야 합니다. server.js 의 데이터베이스 접속 정보가 일치하는지 꼭 확인해주십시오
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,42 @@
 module.exports = function (app, conn) {
-  
+  /* ---------------------------------------------------------- MANAGE */
+  // 테이블 리스트 관리 페이지
+  app.get(['/', '/manage/table_list'], function (req, res) {
+    let tables = [];
+    let show_all_table_sql = 'SHOW TABLES';
+    conn.query(show_all_table_sql, function (err, results) {
+      if (err) {
+        console.log(err);
+        res.send(`<script>alert(\`${err.sqlMessage}\`)</script>`);
+      } else {
+        for (let result of results) {
+          tables.push(result['Tables_in_bookdatabase']);
+        }
+        res.render('index', {
+          page: 'manage/table_list',
+          tables: tables,
+        });
+      }
+    });
+  });
+
+  // 테이블 관리 페이지
+  app.get('/manage/table/:table_name', function (req, res) {
+    let table_name = req.params.table_name;
+    let select_all_sql = `SELECT * FROM \`${table_name}\`;`;
+    let rows;
+    conn.query(select_all_sql, function (err, results) {
+      if (err) {
+        console.log(err);
+        res.send(`<script>alert(\`${err.sqlMessage}\`)</script>`);
+      } else {
+        rows = results;
+      }
+      res.render('index', {
+        page: 'manage/table',
+        table_name: table_name,
+        rows: rows,
+      });
+    });
+  });
 }
